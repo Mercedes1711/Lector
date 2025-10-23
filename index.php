e4840341ba77f4158780767c1ea991e1f2446ee7
Adrian <adrareveg@alu.edu.gva.es>
2025-10-23 20:53:03 +0200

    creacion del form

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
     <title>projecto alejandria</title>
 </head>
 <body>
-    
+  <form action= <?php echo $_SERVER['phpSELF'];?> method= "POST">
+    <label>usuario</label>
+    <input name= "usuario" type="text">
+    <label>contraseña</label>
+    <input name= "contraseña" type="password">
+    <label>correo</label>
+    <input name= "correo" type="text">
+    <input type= buttom>  
 </body>
 </html>
